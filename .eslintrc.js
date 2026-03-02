7e010a73ec41a449b1966224c899545a8aa3d931
Luc Boten <ton.email@exemple.com>
2026-03-02 11:15:57 +0100

    chore: Nettoyage complet du projet
    Suppression de 158 fichiers inutiles :
    - 90+ fichiers .md (résumés de sessions, guides AI générés)
    - 41 scripts one-off (fix encodage, migration, diagnostics)
    - 6 scripts de déploiement à la racine
    - 7 images sources hors public/ (non servables par Next.js)
    - Artefacts de build (tsconfig.tsbuildinfo, lighthouse report 720KB)
    - Templates .env redondants (.env.example, .env.production.template)
    - Fichier test public/test-images.html
    - Config ESLint dupliquée (.eslintrc.js)
    
    Conservés : code source, configs, scripts utiles, seeds, public/

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['next/core-web-vitals'],
-  rules: {
-    'react/no-unescaped-entities': 'off',
-    '@next/next/no-img-element': 'off',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
-};
